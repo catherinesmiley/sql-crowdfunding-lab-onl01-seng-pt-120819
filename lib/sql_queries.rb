c3065ef68d972ffbaf6da2a0593847d8a7124e10
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-25 03:47:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT Projects.title, (Projects.funding_goal - SUM(Pledges.amount)) AS amount_over_goal
+  "SELECT Projects.title, (Projects.funding_goal - SUM(Pledges.amount)) * -1 AS amount_over_goal
   FROM Projects
   LEFT OUTER JOIN Pledges
   ON Projects.id = Pledges.project_id
